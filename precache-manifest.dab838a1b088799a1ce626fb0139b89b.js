46c700eef13b7cedfa127535a61980d61a2b353e
Greg DiCristofaro <gregdicristofaro@gmail.com>
2020-04-10 20:23:55 -0400

    Updates

---
+++ precache-manifest.dab838a1b088799a1ce626fb0139b89b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfec6e25fa565538c8dd12dea7ee7ebb",
+    "url": "/MarylandMetrics/index.html"
+  },
+  {
+    "revision": "fd8c2124debdd37d1b5b",
+    "url": "/MarylandMetrics/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "343c75746c5d47fed1dd",
+    "url": "/MarylandMetrics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd8c2124debdd37d1b5b",
+    "url": "/MarylandMetrics/static/js/2.c4750b9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MarylandMetrics/static/js/2.c4750b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343c75746c5d47fed1dd",
+    "url": "/MarylandMetrics/static/js/main.8e4604c9.chunk.js"
+  },
+  {
+    "revision": "0634cb7fb73e33e2c7a3",
+    "url": "/MarylandMetrics/static/js/runtime-main.3e998c91.js"
+  }
+]);
